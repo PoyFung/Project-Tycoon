9c0e238e7363d08c7a63354174a0998cf714da11
Brandon Tan <btan62747@gmail.com>
2025-07-22 13:55:33 -0400

    Update

--- Project-Tycoon/Assets/Scripts/MainTycoon.cs
+++ Project-Tycoon/Assets/Scripts/MainTycoon.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
-public class hand
+public class player
 {
     [SerializeField]
+    public Dictionary<string,card> playerHand = new Dictionary<string,card>(); //Player's current hand
     public string playerType; //Current user or Opponents?
     public int numCards; //The numbers of cards currently in hand
 }
 
 public class MainTycoon : MonoBehaviour
 {
-    CreateDeck createDeckScript = new CreateDeck();
     [SerializeField] private GameObject playHand; //Player
 
     private void Awake()
