0b78351baa50c5a4e1cd0299feaaf1c178d70d5e
Brandon Tan <btan62747@gmail.com>
2025-07-21 21:38:54 -0400

    Update

--- Project-Tycoon/Assets/Scripts/MainTycoon.cs
+++ Project-Tycoon/Assets/Scripts/MainTycoon.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class MainTycoon : MonoBehaviour
 {
+    [SerializeField] private GameObject testObject;
+
+    private int totalCards = 52;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
