65ba3b8bc80b008125eba0dbda2966caac06ecae
Brandon Tan <btan62747@gmail.com>
2025-07-21 21:49:17 -0400

    Update

--- Project-Tycoon/Assets/Scripts/MainTycoon.cs
+++ Project-Tycoon/Assets/Scripts/MainTycoon.cs
@@ -4,7 +4,15 @@ public class MainTycoon : MonoBehaviour
 {
     [SerializeField] private GameObject testObject;
 
-    private int totalCards = 52;
+    private int totalCards = 54;
+    /* 
+    13 CLUBS
+    13 SPADES
+    13 HEARTS
+    13 DIAMONDS
+    2 Jokers
+     */
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
